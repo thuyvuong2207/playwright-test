c448f27f2eb136d5356965a2d8864b81ccb30da4
vmt <vuongminhthuy97@gmail.com>
2025-06-24 16:31:20 +0700

    Init commit

---
+++ tests/demo.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+import { link } from 'fs';
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://tokecannabis.breadstackcrm.com/');
+});
+
+test('check 1', async ({ page }) => {
+  await page.getByRole('textbox', { name: 'Password:' }).fill('breadstack');
+  await page.getByRole('button', { name: 'Enter' }).click();
+
+  const isAgeGateVisible = await page.locator('#age-gate-modal-content').isVisible();
+  if (isAgeGateVisible) {
+    await page.locator('#age-gate-modal-content').isVisible();
+    await page.locator('warning-msg').isVisible();
+    await page.getByRole('button', { name: 'No, I\'m not' }).click();
+    await page.locator('#err-msg-age').isVisible();
+    await expect(page.locator('#err-msg-age')).toHaveText('We only sell to adults age 19 years or older.');
+
+    expect(await page.locator('.logo a').getAttribute('title')).toBe('Toke Cannabis - Recreational Weed Dispensary With Fast Delivery');
+    expect(await page.locator('.logo a img').first().getAttribute('src')).toBe('https://tokecannabis.breadstackcrm.com/wp-content/uploads/2025/06/Toke-Logo.svg');
+  } else {
+    expect(await page.locator('.logo a').getAttribute('title')).toBe('Toke Cannabis - Recreational Weed Dispensary With Fast Delivery');
+    expect(await page.locator('.logo a img').first().getAttribute('src')).toBe('https://tokecannabis.breadstackcrm.com/wp-content/uploads/2025/06/Toke-Logo.svg');
+  }
+
+});
